684390f0c5438c1be1dd88775556244098e12278
Grant Marshall <grantrmarshall97@gmail.com>
2021-10-13 15:03:58 -0700

    minor updates following new project

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # Tru Exam Schedule Scraper
-Tool to scrap Thompson Rivers University Exam Schedule and format it into JSON.
+Manual tool to scrap Thompson Rivers University Exam Schedule and format it into JSON.
+### Requirements
  Beautiful Soup 4(bs4) 
  requests
- ~
- sys.argv
  
- # how to use
- input or pipe a name without extension to the script as a argument.
- eg python3 finalExamSchedule.py exams yields exams.json
+ # Usage
+ input or pipe a name without extension to the script as a argument.  
+ eg 
+ ``` 
+ python3 finalExamSchedule.py exams
+ 
+ ```
+ returns exams.json
+
+## Important note:
+The tool will be maintained within the examapi repository as a function of that project. This Repository will remain open.
--- finalExamSchedule.py
+++ finalExamSchedule.py
@@ -29,19 +29,18 @@ def extract_schedule():
 def save_to_file(table, filename):
     out = open(filename+".json", 'w')
     # for formatting of courses in Json, fairly standard 4 indent
-    j = json.dumps(table, indent=4)
-    print(j, file=out)
-
-
-def api_json():
-    courselist = extract_schedule()
-    return json.dumps(courselist, indent=4)
+    json_file = json.dumps(table, indent=4)
+    print(json_file, file=out)
+    out.close()
 
 
 if __name__ == "__main__":
     courselist = extract_schedule()  # get dict
-    filename = argv[1]  # pull filename from CLI
-    if filename is not None:
+    try:
+        filename = argv[1]  # pull filename from CLI
         save_to_file(courselist, filename)  # publish dict as a json
-    else:
-        print("Please add filename argument")
+    except Exception as e:
+        print('likely you forgot the filename argument eg "winter2020" the extension .json is appended')
+        print('stack trace: ')
+        print(e)
+        
